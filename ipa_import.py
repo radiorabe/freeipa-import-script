2dcda4674303bb36628ca1961498fbb13f3270ed
Marco Schmalz <marco@schess.ch>
2016-06-09 20:36:58 +0200

    Generate correct group names

--- ipa_import.py
+++ ipa_import.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
-from __future__ import print_function
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
 
 import csv
 import collections
 import os
+import re
 import subprocess
 import sys
+import unicodedata
+
+
+_groupname_strip_re = re.compile(r'[^A-Za-z0-9_/-]')
 
 ##################
 # Config Section #
@@ -19,7 +25,7 @@ GROUP_SEP = '/'
 
 # Mapping CSV file column number to named properties
 CSV_MAP = {
-    'member_of_groups': 4,
+    'member_of_groups': 12,
     'user_login': 5,
     'first_name': 6,
     'last_name': 7,
@@ -57,6 +63,25 @@ def read_csv_file(filename):
             yield entry
 
 
+def fix_csv_group_names(entries):
+    for entry in entries:
+        original_group_name = entry['member_of_groups'].strip(' ' + GROUP_SEP)
+
+        # Replace spaces and tabs with underscores
+        group_name = '_'.join(original_group_name.split())
+
+        # Replace umlaut characters
+        for umlaut, replacement in zip('äöüÄÖÜ', 'ae oe ue Ae Oe Ue'.split()):
+            group_name = group_name.replace(umlaut, replacement)
+
+        # Strip remaining diacritical marks
+        group_name = unicodedata.normalize('NFKD', group_name)   \
+            .encode('ascii', 'ignore').decode('ascii')
+
+        # Strip all remaining illegal characters
+        group_name = _groupname_strip_re.sub('', group_name)
+        group_names = group_name.split(GROUP_SEP)
+        entry['member_of_groups'] = group_names
 def parse_freeipa_output(output):
     """Parse the output from the FreeIPA command line tool"""
     entry = {}
@@ -107,7 +132,7 @@ def find_user_differences(csv_entries, ipa_entries):
             filter(bool, old.get('member_of_groups', '').split(', '))
         )
         new_groups = set(
-            filter(bool, new.get('member_of_groups', '').split(GROUP_SEP))
+            list(filter(bool, new.get('member_of_groups', [])))
             + DEFAULT_GROUPS
         )
         for group in new_groups - old_groups:
@@ -116,14 +141,17 @@ def find_user_differences(csv_entries, ipa_entries):
             changes['group-remove-member'][group]\
                 .append('--users={}'.format(user))
 
+    return changes
+
 
 def find_group_changes(user_changes):
     """Find newly added groups in changes"""
     changes = collections.defaultdict(list)
-    for group in changes['group-add-member']:
+    for group in user_changes['group-add-member']:
         if subprocess.call(['ipa', 'group-show', group],
                            stdout=DEV_NULL, stderr=DEV_NULL) != 0:
             changes[group] = []
+    return changes
 
 
 def commit_changes(changes):
@@ -138,6 +166,7 @@ def commit_changes(changes):
 
 def main(filename):
     csv_entries = list(read_csv_file(filename))
+    fix_csv_group_names(csv_entries)
     ipa_entries = query_ipa(entry['user_login'] for entry in csv_entries)
 
     changes = find_user_differences(csv_entries, ipa_entries)
